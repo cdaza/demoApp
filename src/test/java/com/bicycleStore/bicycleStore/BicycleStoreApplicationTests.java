ea3b3a5629b2d5034dcb3280028de1382b72abbd
Carlos Daza <you@example.com>
2019-10-22 16:56:10 -0500

    first commit

---
+++ src/test/java/com/bicycleStore/bicycleStore/BicycleStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bicycleStore.bicycleStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
