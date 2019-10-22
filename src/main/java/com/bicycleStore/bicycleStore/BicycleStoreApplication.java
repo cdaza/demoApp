bb46db81fb2b26a07b2cbb6ce366c1ed901b2643
Carlos Daza <you@example.com>
2019-10-22 17:04:26 -0500

    add swagger and hamcrest libraries and enable swagger

--- src/main/java/com/bicycleStore/bicycleStore/BicycleStoreApplication.java
+++ src/main/java/com/bicycleStore/bicycleStore/BicycleStoreApplication.java
@@ -2,8 +2,11 @@ package com.bicycleStore.bicycleStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+@EnableSwagger2
 public class BicycleStoreApplication {
 
 	public static void main(String[] args) {
