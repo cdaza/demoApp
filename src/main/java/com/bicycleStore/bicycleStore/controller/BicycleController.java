663599f02340c75a9e38435a37f20f8f55c21ac5
Carlos Daza <you@example.com>
2019-10-22 17:37:04 -0500

    add select all bicycles feature

--- src/main/java/com/bicycleStore/bicycleStore/controller/BicycleController.java
+++ src/main/java/com/bicycleStore/bicycleStore/controller/BicycleController.java
@@ -3,9 +3,12 @@ package com.bicycleStore.bicycleStore.controller;
 import com.bicycleStore.bicycleStore.model.BicycleModel;
 import com.bicycleStore.bicycleStore.service.BicycleService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -22,5 +25,11 @@ public class BicycleController {
     public ResponseEntity insertUser(@RequestBody BicycleModel bicycle) {
         bicycleService.createUser(bicycle);
         return ok(bicycle);
+    }
+
+    @GetMapping("/bicycles")
+    public ResponseEntity allUsers() {
+        List<BicycleModel> bicycles = bicycleService.findAllUsers();
+        return ok(bicycles);
     }
 }
--- src/main/java/com/bicycleStore/bicycleStore/service/BicycleService.java
+++ src/main/java/com/bicycleStore/bicycleStore/service/BicycleService.java
@@ -4,6 +4,8 @@ import com.bicycleStore.bicycleStore.model.BicycleModel;
 import com.bicycleStore.bicycleStore.repository.BicycleRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BicycleService {
 
@@ -15,5 +17,9 @@ public class BicycleService {
 
     public BicycleModel createUser(BicycleModel bicycle) {
         return bicycleRepository.save(bicycle);
+    }
+
+    public List<BicycleModel> findAllUsers() {
+        return bicycleRepository.findAll();
     }
 }
